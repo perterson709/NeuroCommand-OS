7cfa58c04fc04afc9fa839d59ef1a390373b3890
perterson709 <kelvinpetersonchgp12@gmail.com>
2025-05-04 12:09:15 +0530

    Add files via upload

--- website2/script.js
+++
@@ -1,9 +0,0 @@
-// Optional: Rotate quotes (basic example)
-const quotes = [
-  "Discipline is destiny.",
-  "Execute like a machine, think like a philosopher.",
-  "You don’t rise to the level of your goals — you fall to the level of your systems.",
-];
-
-const quoteText = document.getElementById("quote-text");
-quoteText.textContent = quotes[Math.floor(Math.random() * quotes.length)];
